b6f3e5b2c9d4e32a66e6bd22a68a9b5c5073196b
kuz1 <jan.kuzel@vf.cz>
2015-03-17 23:47:18 +0100

    Sql pro vytvoreni sqlite databaze.

---
+++ LunchVoting/LunchVoting/sql/vflv.sql
@@ -0,0 +1,23 @@
+drop table if exists users;
+create table users (
+	id INTEGER primary key autoincrement,
+	name TEXT unique not null,
+	pass TEXT not null,
+	firstname TEXT default null,
+	surname TEXT default null
+);
+
+drop table if exists pubs;
+create table pubs (
+	id INTEGER primary key autoincrement,
+	title TEXT unique not null
+);
+
+drop table if exists votings;
+create table votings (
+	id INTEGER primary key autoincrement,
+	date INTEGER not null,
+	user TEXT not null,
+	pub TEXT not null,
+	rating INTEGER not null
+);
