a1430af3d362769400190e7ad3901cfb31ccd56c
kuz1 <jan.kuzel@vf.cz>
2015-03-17 20:01:00 +0100

    Odstraneni nepotrebnych stranek a kodu - veci generovane automaticky VS..

--- LunchVoting/LunchVoting/views.py
+++ LunchVoting/LunchVoting/views.py
@@ -9,52 +9,12 @@ from datetime import datetime
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from LunchVoting import app
 
-@app.route('/')
-@app.route('/home')
-def home():
-    """Renders the home page."""
-    return render_template(
-        'index.html',
-        title='Home Page',
-        year=datetime.now().year,
-    )
-
-@app.route('/contact')
-def contact():
-    """Renders the contact page."""
-    return render_template(
-        'contact.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
-    )
-
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template(
-        'about.html',
-        title='About',
-        year=datetime.now().year,
-        message='Your application description page.'
-    )
-
 app.config.update(dict(
     USERNAME='admin',
     PASSWORD='pass'
 ))
 
-@app.route('/voting')
-def voting():
-    if not session.get('logged_in'):
-        abort(401)
-    return render_template(
-             'voting.html',
-             title='Voting',
-             year=datetime.now().year,
-             message='Your voting page.'
-            )
-
+@app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -65,14 +25,25 @@ def login():
             return redirect(url_for('voting'))
     # GET
     return render_template(
-        'login.html', 
+        'login.html',
         title='Login',
         year=datetime.now().year,
-        message='Login page.',
+        logged_in=False,
         error=error)
+
+@app.route('/voting')
+def voting():
+    if not session.get('logged_in'):
+        abort(401)
+    return render_template(
+             'voting.html',
+             title='Voting',
+             year=datetime.now().year,
+             logged_in=True
+            )
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
