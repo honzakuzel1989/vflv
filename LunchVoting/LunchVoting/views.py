9fdcdad0dab5b875c45abd3cdb2d0b7125ad23f1
kuz1 <jan.kuzel@vf.cz>
2015-03-17 22:52:07 +0100

    Presun metod pro prace s db so views.py - prozatim. V database.py nefungovalo - operace mimo context aplikace.

--- LunchVoting/LunchVoting/views.py
+++ LunchVoting/LunchVoting/views.py
@@ -3,23 +3,53 @@ Routes and views for the flask application.
 """
 
 import os
-import LunchVoting.presenters as p
 
+from sqlite3 import dbapi2 as sqlite3
 from datetime import datetime
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-from LunchVoting import app
+from flask import Flask, request, session, redirect, url_for, abort, render_template, flash, g
+from LunchVoting import app, proxy
 
-app.config.update(dict(
-    USERNAME='admin',
-    PASSWORD='pass'
-))
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    init_db()
+    print('Initialized the database.')
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the current application context."""
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+def init_db():
+    """Initializes the database."""
+    db = get_db()
+    with app.open_resource('sql/vflv.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+def check_auth():
+    """Check authorization."""
+    return True
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if p.check_auth(request.form['username'], request.form['password']):
+        if check_auth(request.form['username'], request.form['password']):
             session['logged_in'] = True
             flash('You were logged in')
             return redirect(url_for('voting'))
@@ -47,9 +77,3 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('login'))
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Creates the database structure."""
-    p.init_db()
-    print('Initialized the database structure.')
