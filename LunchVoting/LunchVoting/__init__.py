da53c9d2ec5f35595b7b309b17979b38ce741896
kuz1 <jan.kuzel@vf.cz>
2015-03-20 14:37:56 +0100

    Presun 'secret key' do __init__ (vize - muze se pouzivat bez runserver.py pomoci programu flask)

--- LunchVoting/LunchVoting/__init__.py
+++ LunchVoting/LunchVoting/__init__.py
@@ -4,5 +4,7 @@ The flask application package.
 
 from flask import Flask
 app = Flask(__name__)
+app.secret_key = '\xed\x8e\xea}\x03\xc0\x8c$$\x98\xa4\
+        N\xa8\xb6\xf0k\x8a\x86\xe4\xa9\x19\xb7\x8a$'
 
 import LunchVoting.views
--- LunchVoting/runserver.py
+++ LunchVoting/runserver.py
@@ -14,8 +14,6 @@ if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     PORT = int(os.environ.get('SERVER_PORT', '5555'))
 
-    app.secret_key = '\xed\x8e\xea}\x03\xc0\x8c$$\x98\xa4\
-        N\xa8\xb6\xf0k\x8a\x86\xe4\xa9\x19\xb7\x8a$'
     app.run(host=HOST, port=PORT, debug=True)
     print('Started the server.')
     
