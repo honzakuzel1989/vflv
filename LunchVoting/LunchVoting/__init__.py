16c7a26a21f007bd7fbac7ab850f593e86bca129
kuz1 <jan.kuzel@vf.cz>
2015-03-23 11:46:10 +0100

    Presun secret key do runserver.py a jeho nastaveni pomoci promenne prostredi - jako port a host.

--- LunchVoting/LunchVoting/__init__.py
+++ LunchVoting/LunchVoting/__init__.py
@@ -4,7 +4,5 @@ The flask application package.
 
 from flask import Flask
 app = Flask(__name__)
-app.secret_key = '\xed\x8e\xea}\x03\xc0\x8c$$\x98\xa4\
-        N\xa8\xb6\xf0k\x8a\x86\xe4\xa9\x19\xb7\x8a$'
 
 import LunchVoting.views
--- LunchVoting/runserver.py
+++ LunchVoting/runserver.py
@@ -4,17 +4,21 @@ This script runs the LunchVoting application using a development server.
 """
 
 import os
+
 from LunchVoting import app
 
 if __name__ == '__main__':
-    DEBUG = int(os.environ.get('DEBUG', '0'))
-    PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    DEBUG = int(os.environ.get('VFLV_DEBUG', '0'))
+    PORT = int(os.environ.get('VFLV_SERVER_PORT', '5555'))
+    SECRET_KEY = os.environ.get('VFLV_SECRET_KEY', 
+        '\xed\x8ege\xea}\x03\xc0\x8c$$\x98\xa4N\xa8\xb6\xf0k\x8a\x86\xe4\xa9\x19\xb7\x8a$')
 
     if DEBUG:
         app.debug = True
-        HOST = os.environ.get('SERVER_HOST', 'localhost')
+        HOST = os.environ.get('VFLV_SERVER_HOST', 'localhost')
     else:
         HOST = '0.0.0.0'
 
+    app.secret_key = SECRET_KEY
+    print(' * Starting the server')
     app.run(host=HOST, port=PORT)
-    print('Started the server.')
