ec8312c2fdcc17beaf402540d4d1626ed6657f0f
kuz1 <jan.kuzel@vf.cz>
2015-03-23 11:47:05 +0100

    Pridana inicializace databaze (provedeni sql/vflv.sql skriptu) v pripade ze database file ma nulovou velikost.

--- LunchVoting/LunchVoting/database.py
+++ LunchVoting/LunchVoting/database.py
@@ -3,13 +3,14 @@ Database.
 """
 
 import os
+
 from flask import g
 from sqlite3 import dbapi2 as sqlite3
 from LunchVoting import app
 
 def __connect_db():
     """Connects to the specific database."""
-    rv = sqlite3.connect(os.path.join(app.root_path, 'vflv.db'))
+    rv = sqlite3.connect('vflv.db')
     rv.row_factory = sqlite3.Row
     return rv
     
@@ -17,11 +18,13 @@ def get_db():
     """Opens a new database connection if there is none yet for the current application context."""
     if not hasattr(g, 'sqlite_db'):
         g.sqlite_db = __connect_db()
+        if not os.path.getsize('vflv.db'):
+            __init_db(g.sqlite_db)
     return g.sqlite_db
     
-def init_db():
+def __init_db(db):
     """Initializes the database."""
-    db = get_db()
     with app.open_resource('sql/vflv.sql', mode='r') as f:
         db.cursor().executescript(f.read())
     db.commit()
+    print(' * Initialising the database')
