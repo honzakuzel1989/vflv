142f864c3a8b4b4e47c9fd7adce0fc225c60ef6d
kuz1 <jan.kuzel@vf.cz>
2015-03-23 12:08:53 +0100

    Uprava logiky nastaveni.

--- LunchVoting/runserver.py
+++ LunchVoting/runserver.py
@@ -10,15 +10,11 @@ from LunchVoting import app
 if __name__ == '__main__':
     DEBUG = int(os.environ.get('VFLV_DEBUG', '0'))
     PORT = int(os.environ.get('VFLV_SERVER_PORT', '5555'))
+    HOST = os.environ.get('VFLV_SERVER_HOST', 'localhost')
     SECRET_KEY = os.environ.get('VFLV_SECRET_KEY', 
         '\xed\x8ege\xea}\x03\xc0\x8c$$\x98\xa4N\xa8\xb6\xf0k\x8a\x86\xe4\xa9\x19\xb7\x8a$')
 
-    if DEBUG:
-        app.debug = True
-        HOST = os.environ.get('VFLV_SERVER_HOST', 'localhost')
-    else:
-        HOST = '0.0.0.0'
-
+    app.debug = DEBUG
     app.secret_key = SECRET_KEY
     print(' * Starting the server')
     app.run(host=HOST, port=PORT)
