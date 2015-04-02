0ae3b33a02bb7f0be931bf0d6d9214062740d664
kuz1 <jan.kuzel@vf.cz>
2015-04-02 14:01:41 +0000

    Opravena chyba pri pouziti implicitniho parametru funkce.

--- LunchVoting/LunchVoting/helpers.py
+++ LunchVoting/LunchVoting/helpers.py
@@ -10,13 +10,13 @@ def compute_hash_in_hex(password):
     hex_dig = hash_object.hexdigest()
     return hex_dig
 
-def get_time_in_s(dt=datetime.now()):
+def get_time_in_s():
     dt_trunc = dt.replace(hour=0, minute=0, second=0, microsecond=0)
     dt_in_s = int((dt_trunc - datetime(1970,1,1)).total_seconds())
     return dt_in_s
 
 def get_current_time_in_s():
-    dt_now_in_s = get_time_in_s()
+    dt_now_in_s = get_time_in_s(datetime.now())
     return dt_now_in_s
 
 def get_current_year():
