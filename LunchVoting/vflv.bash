2aab07ed3cadea42a378f8bb7ae32aab07b6d245
honzakuzel1989 <honza.kuzel.1989@gmail.com>
2015-07-03 19:48:59 +0800

    zmena adresares se skriptem

--- LunchVoting/vflv.bash
+++ LunchVoting/vflv.bash
@@ -3,7 +3,7 @@
 export VFLV_SERVER_PORT='2223'
 export VFLV_SERVER_HOST=`ifconfig | awk '/inet addr/{print substr($2,6)}' | head -n 1`
 
-cd /home/ubuntu/Programs/flask/vflv/LunchVoting/
+cd /home/bananapi/vflv/LunchVoting
 python2.7 runserver.py 1>OUT 2>ERR
 cd -
 
